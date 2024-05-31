6c0ba68147df8dc2d36e627ec833dcc8bc6aeabb
Michael Sjöberg <mail@michaelsjoeberg.com>
2024-05-31 18:29:56 +0200

    Update operators.rb

--- rb/operators.rb
+++ rb/operators.rb
@@ -3,10 +3,27 @@ epsilon = 1e-6
 
 # core mathematical operators
     
-def mul(a, b)
-    return a * b
+def mul(x, y)
+    return x * y
+end
+
+def id(x)
+    return x
+end
+
+def add(x, y)
+    return x + y
+end
+
+def neg(x)
+    return -x
+end
+
+def lt(x, y)
+    return x < y ? 1.0 : 0.0
 end
 
 # tests
 
 raise Exception unless mul(3, 4) == 12
+raise Exception unless id(3) == 3
